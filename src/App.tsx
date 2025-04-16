07e572f55d8998fea894b067c33ec8b90a4d2e16
Ian-Bright <ianbrighton1@gmail.com>
2025-04-16 12:21:06 -0600

    feat: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Mail } from 'lucide-react';
+import github from './assets/github.png';
+import linkedin from './assets/linkedin.png';
+import logo from './assets/logo.png';
+
+function App() {
+  return (
+    <div className='h-screen flex flex-col justify-between'>
+      <div className='flex font-galdeano justify-between mx-auto py-4 text-3xl tracking-wider w-1/2'>
+        <div className='cursor-pointer'>Reasearch</div>
+        <div className='cursor-pointer'>Blogs</div>
+        <div className='cursor-pointer'>Careers</div>
+      </div>
+      <div className='flex items-center justify-center'>
+        <div className='font-galdeano tracking-wider text-5xl'>
+          Attested Frontiers
+        </div>
+        <img alt='Logo' className='h-64 w-64' src={logo} />
+      </div>
+      <div className='flex items-center gap-12 justify-center px-10 py-4'>
+        <img alt='Github' className='cursor-pointer h-8 w-8' src={github} />
+        <Mail cursor='pointer' size={36} />
+        <img alt='LinkedIn' className='cursor-pointer h-8 w-8' src={linkedin} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                custom: ['Galdeano', 'galdeano'], // Add your custom font here
+            },
+        },
+    },
+}
