80fe33564cfd6163f3ec6e759f7eccbb4361ce91
Ian-Bright <ianbrighton1@gmail.com>
2025-04-16 13:29:55 -0600

    feat: Add compression for images

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
+import compression from 'vite-plugin-compression2'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Group node_modules code into separate chunks
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    compression({
+      algorithm: 'brotliCompress',
+      exclude: [/\.(br)$/, /\.(gz)$/]
+    }),
+    compression({
+      algorithm: 'gzip',
+      exclude: [/\.(br)$/, /\.(gz)$/]
+    }),
+    react(),
+    tailwindcss()
+  ],
 })
