1a9e7379d8f091b7e9bc46b8f10776da42f20331
Ian-Bright <ianbrighton1@gmail.com>
2025-04-30 13:43:49 -0600

    feat: React router added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Mail } from 'lucide-react';
 import github from './assets/github.png';
 import linkedin from './assets/linkedin.png';
+import { Link } from 'react-router-dom';
 import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className='h-screen flex flex-col justify-between'>
       <header>
-        <nav className='flex font-galdeano justify-between mx-auto py-4 text-3xl tracking-wider w-1/2' aria-label="Main Navigation">
-          <div className='cursor-pointer' role="link" tabIndex={0}>Research</div>
-          <div className='cursor-pointer' role="link" tabIndex={0}>Blogs</div>
-          <div className='cursor-pointer' role="link" tabIndex={0}>Careers</div>
+        <nav
+          className='flex font-galdeano justify-between mx-auto py-4 text-3xl tracking-wider w-1/2'
+          aria-label='Main Navigation'
+        >
+          <Link to='/solutions'>Solutions</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/team'>Team</Link>
         </nav>
       </header>
       <main className='flex items-center justify-center'>
@@ -24,24 +28,28 @@ function App() {
           href='https://github.com/attested-frontiers'
           rel='noreferrer'
           target='_blank'
-          aria-label="Visit our GitHub"
+          aria-label='Visit our GitHub'
         >
           <img alt='GitHub Logo' className='h-8 w-8' src={github} />
         </a>
         <a
           href='mailto:contact@attestedfrontiers.xyz'
-          aria-label="Send us an email"
+          aria-label='Send us an email'
         >
           <Mail cursor='pointer' size={36} />
         </a>
-        <a
+        {/* <a
           href='https://linkedin.com/company/attested-frontiers'
           rel='noreferrer'
           target='_blank'
-          aria-label="Visit our LinkedIn"
+          aria-label='Visit our LinkedIn'
         >
-          <img alt='LinkedIn Logo' className='cursor-pointer h-8 w-8' src={linkedin} />
-        </a>
+          <img
+            alt='LinkedIn Logo'
+            className='cursor-pointer h-8 w-8'
+            src={linkedin}
+          />
+        </a> */}
       </footer>
     </div>
   );
