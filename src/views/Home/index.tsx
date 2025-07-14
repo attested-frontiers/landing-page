7d2ec1ebed285cb5275640d2545d050f904b8fae
Ian-Bright <ianbrighton1@gmail.com>
2025-07-14 12:28:53 -0600

    feat: Site title and logo mobile friendly

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,11 +2,15 @@ import logo from '../../assets/logo.png';
 
 export default function HomeView() {
   return (
-    <div className='flex items-center'>
-      <h1 className='font-galdeano tracking-wider text-5xl'>
+    <div className='flex flex-col md:flex-row items-center gap-4 md:gap-0'>
+      <img
+        alt='Attested Frontiers Logo'
+        className='h-32 w-32 md:h-64 md:w-64 order-1 md:order-2'
+        src={logo}
+      />
+      <h1 className='font-galdeano tracking-wider text-3xl md:text-5xl text-center md:text-left order-2 md:order-1'>
         Attested Frontiers
       </h1>
-      <img alt='Attested Frontiers Logo' className='h-64 w-64' src={logo} />
     </div>
   );
 }
